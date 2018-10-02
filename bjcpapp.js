91c1065b9c43b43a932031fc1d6320be3121b996
Liz Steak <lizmaepark@gmail.com>
2018-10-02 11:44:10 -0600

    beer array working correctly

--- bjcpapp.js
+++ bjcpapp.js
@@ -248,15 +248,12 @@ other: [{
 // ===========================
 // TIMERS HERE  
 // 1 CONTINUOUSLY RUNNING GAME TIMER COUNTING UP
+// TIMER RESETS WITH EACH NEW LEVEL - 2 MIN PER LEVEL
 // IDEAL: 10 SECOND TIMER PER TURN - IF TIMER REACHES 0 (COUNTING DOWN), THE GAME IS OVER
-// TIMER RESETS WITH EACH NEW QUESTION
 // ===========================
 
-
-// 1. Intro screen  - (Rules or intro message) and Start Button
-// 2. Clicking start button -> -hides start button, -shows reset button, -starts timer, -  shows question text div and answer buttons
 // 3. Allow user to either click Reset, A or B button -> reset could refresh page (easiest) or stop timer, hide timer, hide question, 
-//    and answer buttons,  and show start button (harder)
+//    and answer buttons and show start button (harder)
 // 4. Have a correct answer (A or B) lead to showing a text that says "You win" or "You lose,"
 // ONCE THE START BUTTON IS CLICKED
 // 3. the program picks 2 random beers
@@ -272,24 +269,56 @@ const game = {
     start() {
         const gameTimer = setInterval (() => {
             this.timer++
-            this.levelTimer++
+            this.levelTimer-=
+            $('.game-timer').text('Time Left: ' + ($('this.leveltimer'))); // timer not showing up on the page yet
                 if(this.levelTimer === 200) {
                 alert('game over')
                 clearInterval(gameTimer)
-                // page is timing out with turn timer only
             }
         }, 1000);
     }
+}
+// beers need to be put into an array so the function can loop through an array
+const beersArray = []
+for(key in beer) {
+    let ciceroneList = beer[key]
+    for(let i = 0; i < ciceroneList.length; i++) {
+        beersArray.push(ciceroneList[i])
+    }
+}
+console.log(beersArray)
+
+let abv1 = console.log(beersArray.splice(Math.floor(Math.random() * beersArray.length), 1)[0])
+let abv2 = console.log(beersArray.splice(Math.floor(Math.random() * beersArray.length), 1)[0])
+
+for(let i = 0; i < beersArray[i].abv.length; i++) {
+    if(abv1 > abv2 && selection === abv1) {
+        alert('beer me')
+    } else {
+        alert('keep drinking')
+    }  
 }
 
 $('#start').on('click', function() {
     //turn off the start button once the game has started
     game.start();
-    $('#start').off() 
+    $('#start').off().hide();
     $('.question_box').text('Which ABV is higher?');
-    $('.row-2').append($('<input type="button" value="test">'));
-    $('.row-2').append($('<input type="button" value="test">'));
+    $('.row-2').append($('<input type="button" value="test" id="option1">'));
+    $('.row-2').append($('<input type="button" value="test" id="option2">'));
+    
+
 })
+
+
+// abvRandom = (abv1, abv2, selection) => {
+//     if(abv1 > abv2 && selection === abv1) {
+//         alert('beer me')
+//     } else {
+//         alert('keep drinking')
+//     }
+// }
+console.log('rando')
 
 // ===========================
 // GAME LOGIC HERE
@@ -300,40 +329,16 @@ $('#start').on('click', function() {
 // Level 3 is to pick the correct fermentation 
 // ===========================
 
-// have a question show up on index.html with 2 buttons for random beer options
-
-// beers need to be put into an array so the function can loop through an array
-// const newArray = Object.entries(beer);
-// abvQuestions = ["Which beer has a higher ABV?"]; 
-// // console.log(newArray)
-
-// // when a choice is clicked, compare the two beers. when the beer is selected correctly, push it into a used array
-// abvQuestion = (abv1, abv2, selection) => {
-//     for(let i = 0; i < newArray.length; i++)
-//     if(abv1 > abv2 && selection === abv1) {
-//         return('beer me')
-//     } else {
-//         return('keep drinking')
-//     }
-// }
+// when a choice is clicked, compare the two beers. when the beer is selected correctly, push it into a used array
+// currently picking which beer has a higher ABV. Beer names will show up, not the ABV
 
 // append the choices to the index.html buttons and add a function for the beers and the selection (beer 1, beer 2, selection)
-
-
-
 // reset the beer array at the end of level 1
-
-
-
-
-// append this question to the page for the whole of level 1
-            // for/if loop. loop through the array and pick a random question. Loop through the beer object and pick 2 random 
-            // different beers. The user has to pick which beer has a higher ABV (option 1 or option 2)
-
 
 // ===========================
 // ADDITIONAL LEVELS HERE
 // ===========================
+// abvQuestions = ["Which beer has a higher ABV?"]; // keep this for use when things get really random
 // colorQuestions = [{question: "What color is most commonly associated with this beer?",
 //                   choices: ["straw", "light gold", "gold", "light amber", "amber", "dark amber", "brown", "dark brown", "black", "varies"],
 //                   }]; // loop through the beer object and pick 1 beer. loop through the colorQuestions array and pick 2 random different 
@@ -343,3 +348,17 @@ $('#start').on('click', function() {
 //                          choices: ["top", "bottom", "wild", "any"],
                         // }]; // loop through the beer object and pick 1 beer. Loop through the fermentationQuestions array and pick 2
                         // random different choices. One of the choices will match the beer given
+
+// for(let i = 0; i < newArray.length; i++){
+//     // print out all the abv beers
+//     console.log(newArray[i].name);
+// }
+
+//     abvQuestion = (abv1, abv2, selection) => {
+//     for(let i = 0; i < newArray.length; i++)
+//     if(abv1 > abv2 && selection === abv1) {
+//         return('beer me') // pop up within game
+//     } else {
+//         return('keep drinking') // pop up within game that resets the level
+//     }
+// }
