50dcc16dcfff6f758e1041894fa1e8758fdf6f2a
Liz Steak <lizmaepark@gmail.com>
2018-10-03 14:37:33 -0600

    game logic working

--- bjcpapp.js
+++ bjcpapp.js
@@ -1,8 +1,20 @@
+// have a question box that will rotate questions in and out
+// create a timer for each turn
+// create 2 categories for each answer
+// have a points board for 1 player and 2 player
+// utilize bootstrap
+// utilize velocity for the categories. different velocity options if the answer is right vs wrong
+// utilize velocity for the question box
+// game reset button 
+
 // ===========================
 // OBJECTS HERE
 // used Cicerone beers & categorization
 // IDEAL: add BJCP beers and origins, consider the varying colors, ABV and IBUs for all beers
 // ===========================
+console.log('so much beer')
+$('#answer').hide();
+$('#gamerules').hide();
 const beer = {
 belgian: [{
         name: 'Gueuze',
@@ -244,38 +256,34 @@ other: [{
         abv: 4.6,
         fermentation: 'bottom',
     }],
-}
+} 
 // ===========================
-// TIMERS HERE  
+// TIMERS & GAME LOGIC HERE  
 // 1 CONTINUOUSLY RUNNING GAME TIMER COUNTING UP
 // TIMER RESETS WITH EACH NEW LEVEL - 2 MIN PER LEVEL
+// Level 1 is which beer has the higher ABV
+// Level 2 is to pick the correct color of beer
+// Level 3 is to pick the correct fermentation 
+// IDEAL: HAVE 2 QUESTIONS FOR LEVEL 1 - HIGHER AND LOWER ABV
 // IDEAL: 10 SECOND TIMER PER TURN - IF TIMER REACHES 0 (COUNTING DOWN), THE GAME IS OVER
-// ===========================
-
-// 3. Allow user to either click Reset, A or B button -> reset could refresh page (easiest) or stop timer, hide timer, hide question, 
-//    and answer buttons and show start button (harder)
-// 4. Have a correct answer (A or B) lead to showing a text that says "You win" or "You lose,"
-// ONCE THE START BUTTON IS CLICKED
-// 5. when you click the button, the console logs which beer they chose
-// 6. a function runs that compares the two beers
-// 7. the result gets appended to the page
+// IDEAL: EASY, INTERMEDIATE AND ADVANCED. 90 SEC PER LEVEL OF INTERMEDIATE. 45 SEC FOR ADVANCED
+// ============================
 const game = {
-    level: 1,
-    score: 0,
     timer: 0,
     levelTimer: 0,
     start() {
         const gameTimer = setInterval (() => {
             this.timer++
-            this.levelTimer-=
-            $('.game-timer').text('Time Left: ' + ($('this.leveltimer'))); // timer not showing up on the page
-                if(this.levelTimer === 200) {
-                alert('game over')
+            this.levelTimer++
+            $('#game-timer').text('Timer: ' + this.levelTimer);
+            // console.log(this.levelTimer) // returning NaN with -=
+                if(this.levelTimer === 120) {
+                alert('You\'re cut off')
                 clearInterval(gameTimer)
-            }
-        }, 1000);
-    }
-}
+            } 
+        }, 1000); 
+    } 
+} 
 // beers need to be put into an array so the function can loop through an array
 const beersArray = []
 for(key in beer) {
@@ -284,42 +292,73 @@ for(key in beer) {
         beersArray.push(ciceroneList[i])
     }
 }
-console.log(beersArray)
 
+let beer1 = '';
+let beer2 = '';
+points = 0;
+level = 1;
 $('#start').on('click', function() {
-    //turn off the start button once the game has started
     game.start();
-    $('#start').off().hide();
+    $('#start').off()
+    $('#gamerules').hide();
     $('.question_box').text('Which ABV is higher?');
-    $('.row-2').append($('<input type="button" value="test" class="btn btn-outline-light" aria-pressed="true" id="option1">'));
-    $('.row-2').append($('<input type="button" value="test" class="btn btn-outline-light" aria-pressed="true" id="option2">'));
-    let abv1 = beersArray.splice(Math.floor(Math.random() * beersArray.length), 1)[0] 
-    let abv2 = beersArray.splice(Math.floor(Math.random() * beersArray.length), 1)[0]
-    let selection;
-    $('#option1').val(abv1.name)
-    $('#option2').val(abv2.name)
-    for(let i = 0; i < beersArray.length; i++) {
-    if(abv1 > abv2 && selection === abv1) {
-        alert('beer me')
-    } else {
-        alert('keep drinking')
-        break
-        } 
-    }
+    $('#options').append($('<input type="button" value="test" class="btn btn-outline-light" aria-pressed="true" id="option1">'));
+    $('#options').append($('<input type="button" value="test" class="btn btn-outline-light" aria-pressed="true" id="option2">'));
+    beer1 = beersArray.splice(Math.floor(Math.random() * beersArray.length), 1)[0];
+    beer2 = beersArray.splice(Math.floor(Math.random() * beersArray.length), 1)[0];
+    $('#option1').val(beer1.name)
+    $('#option2').val(beer2.name)
+
+    $('#option1, #option2').on('click', function(e) {
+      let userChoice = $(e.target).attr('id')
+      let otherChoice = {}
+      if(userChoice === "option1"){
+        userChoice = beer1
+        otherChoice = beer2
+        console.log(userChoice.name)
+      } else {
+        userChoice = beer2
+        otherChoice = beer1
+        console.log(userChoice.name)
+      }
+      if(userChoice.abv > otherChoice.abv){
+            points++
+            $('#score').text(points)
+            $('#option1, #option2').off()
+            $('#answer').show()
+      $('.option_response').text(`You chose ${userChoice.name}. You are correct!`); // New beers populate in
+        } else {
+            $('.option_response').text('Try again') // New beers populate in and you get to try again 
+        }
+    })
+    
 })
-console.log('rando')
+// when resetting use options.empty()
+$('#beerme').on('click', function () {
+  $('#answer').hide()
 
-// ===========================
-// GAME LOGIC HERE
-// LOOP THROUGH AN ARRAY OF BEER
-// Level 1 is which beer has the higher ABV
-// IDEAL: HAVE 2 QUESTIONS FOR LEVEL 1
-// Level 2 is to pick the correct color of beer
-// Level 3 is to pick the correct fermentation 
-// ===========================
+})
 
-// when a choice is clicked, compare the two beers. when the beer is selected correctly, push it into a used array
-// currently picking which beer has a higher ABV. Beer names will show up, not the ABV
+$('#cutoff').on('click', function () {
+  $('#answer').hide()
+
+})
+
+$('#rulesbutton').on('click', function () {
+    $('#gamerules').show()
+    // need to add in a hide function when the div or outside the div is clicked
+})
+
+$('#cutoff').on('click', function() {
+    $('#answer').hide()
+    document.location.reload()
+  })
+
+$('#reset').on('click', function() {
+  document.location.reload()
+})
+
+console.log('the end')
 
 // append the choices to the index.html buttons and add a function for the beers and the selection (beer 1, beer 2, selection)
 // reset the beer array at the end of level 1
@@ -327,6 +366,8 @@ console.log('rando')
 // ===========================
 // ADDITIONAL LEVELS HERE
 // ===========================
+// 3. Allow user to either click Reset, A or B button -> reset could refresh page (easiest) or stop timer, hide timer, hide question, 
+//    and answer buttons and show start button (harder)
 // abvQuestions = ["Which beer has a higher ABV?"]; // keep this for use when things get really random
 // colorQuestions = [{question: "What color is most commonly associated with this beer?",
 //                   choices: ["straw", "light gold", "gold", "light amber", "amber", "dark amber", "brown", "dark brown", "black", "varies"],
@@ -337,17 +378,3 @@ console.log('rando')
 //                          choices: ["top", "bottom", "wild", "any"],
                         // }]; // loop through the beer object and pick 1 beer. Loop through the fermentationQuestions array and pick 2
                         // random different choices. One of the choices will match the beer given
-
-// for(let i = 0; i < newArray.length; i++){
-//     // print out all the abv beers
-//     console.log(newArray[i].name);
-// }
-
-//     abvQuestion = (abv1, abv2, selection) => {
-//     for(let i = 0; i < newArray.length; i++)
-//     if(abv1 > abv2 && selection === abv1) {
-//         return('beer me') // pop up within game
-//     } else {
-//         return('keep drinking') // pop up within game that resets the level
-//     }
-// }
